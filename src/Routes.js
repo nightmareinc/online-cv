e52ce3d799a172ca308a667c14683231bf3689cf
Hossein Azadi <hossein.azadi14@outlook.com>
2019-03-22 17:02:48 +0430

    added resume Route

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import Layout from './HOC/Layout';
+
+// components
 import Home from './containers/App/Home';
+import Resume from './containers/App/Resume';
 
 const Routes = () => {
     return (
         <Layout>
             <Switch>
                 <Route path='/' exact component={Home} />
+                <Route path='/resume' exact component={Resume} />
             </Switch>
         </Layout>
     );
