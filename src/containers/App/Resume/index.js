99641dd03abb9414d58c6b43b2695cdd9a48c714
Hossein Azadi <hossein.azadi14@outlook.com>
2019-03-23 15:20:53 +0430

    data fetching and state sync finished

--- src/containers/App/Resume/index.js
+++ src/containers/App/Resume/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import { URL } from '../../../config/statics';
 
 class Resume extends Component {
+    state = {
+        resumeData: {},
+        loading: true
+    }
+
+    componentWillMount(){
+        console.log('loading',this.state.loading)
+        setTimeout(() => {
+            Axios.get(`${URL}/resume`)
+            .then( res => {
+                this.setState({
+                    resumeData: res.data,
+                    loading: false
+                });
+            });
+        }, 2000);
+        
+    };
+
     render() {
         return (
             <div>
-                resume here
+                {this.state.loading ? <h1> true </h1> : <h1> false </h1>}
             </div>
         );
     }
