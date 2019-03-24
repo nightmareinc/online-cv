629074810bf6331660c5d46c43364a4cd1cd197f
Hossein Azadi <hossein.azadi14@outlook.com>
2019-03-24 11:54:25 +0430

    improved global styles

--- src/config/GLobalStyles.js
+++ src/config/GLobalStyles.js
@@ -1,6 +1,9 @@
 import styled, {createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+    }
    body {
        margin: 0;
        padding: 0;
