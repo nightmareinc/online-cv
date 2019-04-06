484bd43da30788e8d421772771491a8888564d30
Hossein Azadi <hossein.azadi14@outlook.com>
2019-04-06 13:09:22 +0430

    added 404 route

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Link} from 'react-router-dom';
 import Layout from './HOC/Layout';
 
 // components
 import Home from './containers/App/Home';
 import Resume from './containers/App/Resume';
-import Protfolio from './containers/App/Portfolio';
+import Portfolio from './containers/App/Portfolio';
 import Personality from './containers/App/Personality';
 import ContactMe from './containers/App/ContactMe';
 import Projects from './containers/App/Projects';
 import Skills from './containers/App/Skills';
+
+const NoMatch = () => {
+    return (
+        <div style={{
+            textAlign: 'center',
+            color: '#d3d6db'
+        }}>
+            <div>
+                <h1 style={{
+                    fontSize: '124px',
+                    letterSpacing: '10px'
+                }}>404</h1>
+            </div>
+            <h2>Oops! Page Not Found</h2>
+            <Link to='/' style={{
+                color: '#d3d6db'
+            }}>Back To Homepage</Link>
+        </div>
+    )
+}
 
 const Routes = () => {
     return (
@@ -17,11 +37,12 @@ const Routes = () => {
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/resume' exact component={Resume} />
-                <Route path='/portfolio' exact component={Protfolio} />
+                <Route path='/portfolio' exact component={Portfolio} />
                 <Route path='/personality' exact component={Personality} />
                 <Route path='/contactme' exact component={ContactMe} />
                 <Route path='/projects' exact component={Projects} />
                 <Route path='/skills' exact component={Skills} />
+                <Route component={NoMatch} />
             </Switch>
         </Layout>
     );
