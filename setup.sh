81f0e53cd28af8d1e8135e6f54d61a30a3828f68
Brian Salehi <salehibrian@gmail.com>
2020-07-05 00:18:42 +0430

    setup script coloring scheme added

--- setup.sh
+++ setup.sh
@@ -3,12 +3,20 @@
 root_directory=hosseinazadi
 service=server.js
 port=9000
+# color codes
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+BOLD="\e[1m"
+BLINK="\e[5m"
+STRIKE_THROUGH="\e[9m"
+DEFAULT="\e[39m\e[49m\e[0m"
 
 if [[ $UID -eq 0 ]]; then
-    echo "you cannot be root, run by an unprivilaged user"
+    echo -e $BOLD"you cannot be root, run by an unprivilaged user"$DEFAULT
     exit 1
 else
-    echo "running, this might take a few seconds..."
+    echo -e $BLUE"running, this might take a few seconds..."$DEFAULT
 fi
 
 pid=$(netstat -antp 2>/dev/null | grep -E "8000.*\/node" | sed -r 's/^.*\ ([0-9]*)\/node.*$/\1/')
@@ -16,30 +24,30 @@ pid=$(netstat -antp 2>/dev/null | grep -E "8000.*\/node" | sed -r 's/^.*\ ([0-9]
 if [[ -n $pid ]]; then
     sudo kill $pid 2>/dev/null
     if [[ $? -eq 0 ]]; then
-        echo "process $pid was killed on port $port"
+        echo -e $BLUE"process $pid was killed on port $port"$DEFAULT
     else
-        echo "failed to kill process $pid, something went wrong!"
+        echo -e $BOLD"failed to kill process $pid, something went wrong!"$DEFAULT
         exit 3
     fi
 else
-    echo "no services are already running! skipping..."
+    echo -e $BLUE"no services are already running! skipping..."$DEFAULT
 fi
 
-echo "installing node packages, this might take a few seconds..."
-npm install &>/tmp/setup.log
+echo -e $BLUE"installing node packages, this might take a few seconds..."$DEFAULT
+sudo npm install &>/tmp/setup.log
 if [[ $? -ne 0 ]]; then
-    echo "something went wrong while installing npm packages, cannot ignore it"
+    echo -e $BOLD"something went wrong while installing npm packages, cannot ignore it"$DEFAULT
     echo "log -> /tmp/setup.log"
     exit 5
 else
-    echo "npm packages installed"
+    echo $GREEN"npm packages installed"$DEFAULT
 fi
 
 node $service &
 if [[ $? -eq 0 ]]; then
-    echo "services are running :)"
+    echo -e $GREEN"service is running in background"$DEFAULT
     if [ -f /tmp/setup.log ]; then rm -f /tmp/setup.log; fi
 else
-    echo "failed to run server, cannot ignore it"
+    echo -e $BOLD"failed to run server, cannot ignore it"$DEFAULT
     exit 6
 fi
