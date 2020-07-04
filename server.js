2f866e1cf0c511f32169852e336bc3b18d9efefe
Brian Salehi <salehibrian@gmail.com>
2020-07-05 01:41:32 +0430

    build directory changed to public in server

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/resume", (req, res) => {
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(9000);
